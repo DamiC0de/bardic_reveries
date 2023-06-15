29418aed08b98fa94c71b7b21c46ab72ee3fba79
Valentin Da silva <v.dasilva11d@gmail.com>
2023-06-15 16:42:27 +0200

    New seed.rb users can no longer delete other users stories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+print "Creating 2 users"
+2.times do
+    name = Faker::Name.first_name
+    password = "#{name}test"
+    user = User.create(pseudo:name,email:"#{name}@mail",password: password,password_confirmation:password)
+    date = Time.now - rand(0..10).days
+    date_exp = date + rand(1..31).days
+    Subscription.create(user:user,start_date:date,expiration_date:date_exp)
+end
+puts " DONE"
+
+print "Creating 10 stories"
+
+10.times do
+    name = Faker::Name.first_name
+    object = Faker::Games::Minecraft.item
+    title = "#{name} and the #{object}"
+    Story.create(title: title,theme:"fantasy",age: rand(5..10),first_character: name,fav_object: object, content: Faker::Lorem.sentences  ,user:(User.all.sample),secondary_character: Faker::Name.first_name)
+end
+puts  " DONE"
