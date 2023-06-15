b16fe1b235fed3be83df622f9d421a8a92e27547
DamiC0de <dreymidot@gmail.com>
2023-06-15 15:25:24 +0200

    edit routes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,31 @@
 class ProfilesController < ApplicationController
-end
+    before_action :authenticate_user!
+    before_action :set_user
+  
+    def show
+    end
+  
+    def edit
+    end
+  
+    def update
+      if @user.update(user_params)
+        flash[:notice] = "Votre profil a été mis à jour avec succès."
+        redirect_to profile_path(@user)
+      else
+        flash[:alert] = "Il y a eu une erreur lors de la mise à jour de votre profil."
+        render :edit
+      end
+    end
+  
+    private
+  
+    def set_user
+      @user = current_user
+    end
+  
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+  end
+  
