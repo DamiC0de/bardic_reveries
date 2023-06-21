3e05eeafcf3c90fa1cd1414ca455867f2100caa9
DamiC0de <dreymidot@gmail.com>
2023-06-21 10:25:02 +0200

    edit order_controller.rb

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -3,18 +3,27 @@ class OrderController < ApplicationController
   end
 
   def create
-    # Before the rescue, at the beginning of the method
-@stripe_amount = 500begin  customer = Stripe::Customer.create({
-  email: params[:stripeEmail],
-  source: params[:stripeToken],
-  })  charge = Stripe::Charge.create({
-  customer: customer.id,
-  amount: 100,
-  description: "Achat d'un abonnement à nos histoires",
-  currency: 'eur',
-  })rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_order_path
-  end# After the rescue, if the payment succeeded
+    @stripe_amount = 500 # Assurez-vous de définir la bonne valeur ici
+
+    begin  
+      customer = Stripe::Customer.create({
+        email: params[:stripeEmail],
+        source: params[:stripeToken],
+      })  
+      
+      charge = Stripe::Charge.create({
+        customer: customer.id,
+        amount: @stripe_amount,  # Utiliser @stripe_amount défini précédemment
+        description: "Achat d'un abonnement à nos histoires",
+        currency: 'eur',
+      })
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_order_path and return
+    end
+
+    # Après le rescue, si le paiement a réussi
+    # Vous pouvez ajouter ici votre logique pour gérer la réussite du paiement.
+    # Par exemple, vous pourriez rediriger l'utilisateur vers une page de réussite.
   end
 end
