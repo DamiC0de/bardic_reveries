d2fdd9a8e668eada6f36fd546c7470c28001e064
DamiC0de <dreymidot@gmail.com>
2023-06-24 12:51:12 +0200

    edit css

--- config/initializers/blacklist.rb
+++
@@ -1,5 +0,0 @@
-Obscenity.configure do |config|
-    config.blacklist = ['bite ',' baisable ',' baise ',' baiser ',' bander ',' branler ',' branlette ',' bordel ',' burnes ' ,' chiant ',' chiasse ',' chier ',' chiottes ',' con ',' conne ',' connerie ',' coucougnettes ',' couille',' couillu ',' cul ',' déconner ',' déconneur ',' emmerder ',' emmerdant ',' emmerdeur ',' emmerdeuse ',' empapaouter ',' enculer ',' entuber ',' faire chier ',' faire une pipe ',' foutoir ',' foutre ',' foutre le camp ',' foutu(e) ',' gueule ',' gueuler ',' merde ',' merder ',' merdier ',' merdique ',' niquer ',' nique ta mère ',' pisser ',' putain ',' pute ',' roubignoles ',' roupettes ',' roustons ',' se démerder ',' s’emmerder ',' se faire chier ',' se faire sauter ',' sucer ',' ta gueule',' tirer un coup ',' turlutte ',' zigounette ',' zob']
-  end
-
-
